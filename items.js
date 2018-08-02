f2bf4583fbd96a34dea5d01444c194f736addd63
Holden <holden@3jaw.com>
2018-08-02 15:45:16 -0600

    Added passive items

--- items.js
+++ items.js
@@ -16,46 +16,98 @@ class StatItem {
     }
 }
 
+class PassiveItem {
+    constructor(pickedUp, storedNumA, storedNumB, storedNumC, name, desc, img, itemNumber) {
+        this.pickedUp = pickedUp;
+        this.SNA = storedNumA;
+        this.SNB = storedNumB;
+        this.SNC = storedNumC;
+        this.name = name;
+        this.desc = desc;
+        this.img = img;
+        this.itemNumber = itemNumber + STATITEMS.length;
+    }
+}
+
 function gainItem(roomSeed) {
-    let Count = 1, RS= ((floor.seed * roomSeed * Count) % player.Stats.Items.length); 
-    while (player.Stats.Items[RS].pickedUp) {
-        Count++;
-        RS = ((floor.seed * roomSeed * Count) % player.Stats.Items.length);
-        
-        if(Count>25){
-            RS = Math.floor(random(0,player.Stats.Items.length))
+    let Count = 1;
+    let RS = ((floor.seed * roomSeed * Count) % player.Stats.Items.length);
+    if(floor.seed%3==0){
+        while (player.passives[RS].pickedUp) {
+            Count++;
+            RS = ((floor.seed * roomSeed * Count) % player.passives.length);
+
+            if (Count > 25) {
+                RS = Math.floor(random(0, player.passives.length))
+            }
         }
+        player.passives[RS].pickedUp = true;
+    }else{
+        
+        while (player.Stats.Items[RS].pickedUp) {
+            Count++;
+            RS = ((floor.seed * roomSeed * Count) % player.Stats.Items.length);
+
+            if (Count > 25) {
+                RS = Math.floor(random(0, player.Stats.Items.length))
+            }
+        }
+
+        player.Stats.Items[RS].pickedUp = true;
+        player.Stats.reload = Math.floor(player.Stats.reload * player.Stats.Items[RS].reload * 100) / 100;
+        player.Stats.damage = Math.floor(player.Stats.damage * player.Stats.Items[RS].damage * 100) / 100;
+        player.MAXHP = Math.floor(player.MAXHP * player.Stats.Items[RS].health * 100) / 100;
+        if (player.Stats.Items[RS].health > 1) {
+            player.HP = player.MAXHP;
+        }
+        player.speed = Math.floor(player.speed * player.Stats.Items[RS].speed * 100) / 100;
+        player.Stats.speed = Math.floor(player.Stats.speed * player.Stats.Items[RS].bulletSpeed * 100) / 100;
+        player.Stats.range = Math.floor(player.Stats.range * player.Stats.Items[RS].range * 100) / 100;
+        player.Stats.size = Math.floor(player.Stats.size * player.Stats.Items[RS].size * 100) / 100;
+        player.Stats.maxAmmo = Math.floor(player.Stats.maxAmmo * player.Stats.Items[RS].ammo * 100) / 100;
+
+
+
+        player.Stats.newestItem = player.Stats.Items[RS];
+        player.Stats.itemReceiveTime = interval;
     }
-
-    player.Stats.Items[RS].pickedUp = true;
-    player.Stats.reload = Math.floor(player.Stats.reload * player.Stats.Items[RS].reload * 100) / 100;
-    player.Stats.damage = Math.floor(player.Stats.damage * player.Stats.Items[RS].damage * 100) / 100;
-    player.MAXHP = Math.floor(player.MAXHP * player.Stats.Items[RS].health * 100) / 100;
-    if (player.Stats.Items[RS].health > 1) {
-        player.HP = player.MAXHP;
-    }
-    player.speed = Math.floor(player.speed * player.Stats.Items[RS].speed * 100) / 100;
-    player.Stats.speed = Math.floor(player.Stats.speed * player.Stats.Items[RS].bulletSpeed * 100) / 100;
-    player.Stats.range = Math.floor(player.Stats.range * player.Stats.Items[RS].range * 100) / 100;
-    player.Stats.size = Math.floor(player.Stats.size * player.Stats.Items[RS].size * 100) / 100;
-    player.Stats.maxAmmo = Math.floor(player.Stats.maxAmmo * player.Stats.Items[RS].ammo * 100) / 100;
-
-
-
-    player.Stats.newestItem = player.Stats.Items[RS];
-    player.Stats.itemReceiveTime = interval;
+    
 }
 
 //-rl-dmg-hp-sp-blsp-rng-sz-ammo-------------------------------------------------
 let STATITEMS = [
-    slickOil = new StatItem(false, 1.25, 1, 1, 1, 1, 1, 1, 1, "Slick Oil", "Bullets slide in like butter",1),
-    fruitsnveggies = new StatItem(false, 1, 1.1, 1.15, 1, 1, 1, 1, 1,"fruits 'n veggies", "Help you grow",2),
-    rollerBlades = new StatItem(false, 1, 1, 1, 1.2, 1, 1, 1, 1, "Roller Blades", "Skating > Walking",3),
-    barrelBloater = new StatItem(false, 1, 1, 1, 1, 1, 1, 1.5, 1,"Barrel Bloater", "Bigger bullets",4),
-    monocle = new StatItem(false, 1, 1, 1, 1, 1, 1.15, 1, 1,"Monocle", "Helps you shoot",5),
-    proteinMix = new StatItem(false, 1, 1, 1.25, 1, 1, 1, 1, 1,"Protein Mix", "Add some protein",6),
-    olympicMedal = new StatItem(false, 1, 1, 1, 1.2, 1.5, 1, 1, 1,"Olympic Medal", "Herculian speeds",7),
-    goldBullets = new StatItem(false, 0.8, 1.5, 1, 1, 0.66, 1, 1, 1,"Gold Bullets", "Touched by the King",8),
-    rocketPropellers = new StatItem(false, 1, 1.05, 1, 1, 1.5, 1.25, 1, 1,"Rocket Propellers", "Requires a rocket scientist",9)
+    slickOil = new StatItem(false, 1.25, 1, 1, 1, 1, 1, 1, 1, "Slick Oil", "Bullets slide in like butter", 0, 1),
+    fruitsnveggies = new StatItem(false, 1, 1.1, 1.15, 1, 1, 1, 1, 1, "fruits 'n veggies", "Help you grow", 0, 2),
+    rollerBlades = new StatItem(false, 1, 1, 1, 1.2, 1, 1, 1, 1, "Roller Blades", "Skating > Walking", 0, 3),
+    barrelBloater = new StatItem(false, 1, 1, 1, 1, 1, 1, 1.5, 1, "Barrel Bloater", "Bigger bullets", 0, 4),
+    monocle = new StatItem(false, 1, 1, 1, 1, 1, 1.15, 1, 1, "Monocle", "Helps you shoot", 0, 5),
+    proteinMix = new StatItem(false, 1, 1, 1.25, 1, 1, 1, 1, 1, "Protein Mix", "Add some protein", 0, 6),
+    olympicMedal = new StatItem(false, 1, 1, 1, 1.2, 1.5, 1, 1, 1, "Olympic Medal", "Herculian speeds", 0, 7),
+    goldBullets = new StatItem(false, 0.8, 1.5, 1, 1, 0.66, 1, 1, 1, "Gold Bullets", "Touched by the King", 0, 8),
+    rocketPropellers = new StatItem(false, 1, 1.05, 1, 1, 1.5, 1.25, 1, 1, "Rocket Propellers", "Requires a rocket scientist", 0, 9)
 ];
 
+let PASSIVEITEMS = [
+    bottledRage = new PassiveItem(false, 1, 0, 0, "Bottled Rage", "My pain is your pain", 0, 1),
+    lotsOfBullets = new PassiveItem(false, 0, 0, 0, "Lots of Bullets", "More Bullets, More Damage", 0, 2),
+    emergencyAmmo = new PassiveItem(false, 2, 0, 0, "Emergency Ammo", "Reloads two empty guns", 0, 3)
+];
+
+function passiveCheck() {
+    if (player.passives[0].pickedUp) { //Bottled Rage
+        player.Stats.damage = Math.floor(player.Stats.damage / player.passives[0].SNA * 100) / 100;
+        player.passives[0].SNA = Math.floor((player.MAXHP - player.HP) * 100) / 1000;
+        player.Stats.damage = Math.floor(player.Stats.damage * player.passives[0].SNA * 100) / 100;
+    }
+    if (player.passives[1].pickedUp) { //Lots of Bullets
+        player.Stats.damage = Math.floor(player.Stats.damage / player.passives[1].SNA * 100) / 100;
+        player.passives[1].SNA = Math.floor((Pbullets.length + Ebullets.length) * 100) / 2000;
+        player.Stats.damage = Math.floor(player.Stats.damage * player.passives[1].SNA * 100) / 100;
+    }
+    if (player.passives[2].pickedUp) { //Emergency Ammo
+        if (player.Stats.CurrentWeapon.ammo == 0 && player.passives[2].SNA > 0) {
+            player.passives[2].SNA -= 1;
+            player.Stats.CurrentWeapon.ammo = player.Stats.CurrentWeapon.maxAmmo
+        }
+    }
+}
