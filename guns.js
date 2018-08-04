e563a4a2e54174f3cf82a1903b75a037f8f83936
Holden <holden@3jaw.com>
2018-08-03 18:46:31 -0600

    Added Seedling shooter gun

--- guns.js
+++ guns.js
@@ -99,7 +99,9 @@ let OneGuns = [
 
     new Gun("Precision Sniper", 0.33, 14, 3, 3, 0.5, "yellow", 30, true, 0),
 
-    new Gun("Rapid Fire RPG", 0.5, 8, 4, 3, 2.5, "yellow", 100, 0, new exploOBJ(60, 2))
+    new Gun("Rapid Fire RPG", 0.5, 8, 4, 3, 2.5, "yellow", 100, 0, new exploOBJ(60, 2)),
+
+    new Gun("Seedling Shooter", 3, 0.75, 1.25, 1, 0.75, "brown", 200, true, false)
 ];
 GodGun = function () {
     return new Gun("God Gun", 60, 150, 6, 10, 8, "yellow", 100000000, 0, 0);
